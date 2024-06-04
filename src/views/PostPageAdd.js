d18d6a609c64fda3efd9671a78ba2a8fd482def5
Jonathan Ong <73453714+ongaikngee@users.noreply.github.com>
2024-06-04 14:15:23 +0800

    preview

--- src/views/PostPageAdd.js
+++ src/views/PostPageAdd.js
@@ -1,24 +1,32 @@
 import React, { useEffect, useState } from "react";
-import { Button, Container, Form, Nav, Navbar } from "react-bootstrap";
+import { Button, Container, Form, Nav, Navbar, Image } from "react-bootstrap";
 import { addDoc, collection } from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
-import { auth, db } from "../firebase";
+import { auth, db, storage } from "../firebase";
 import { signOut } from "firebase/auth";
-
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 export default function PostPageAdd() {
   const [user, loading] = useAuthState(auth);
   const [caption, setCaption] = useState("");
   const [image, setImage] = useState("");
   const navigate = useNavigate();
+  const [previewImage, setPreviewImage] = useState(
+    "https://zca.sg/img/placeholder"
+  );
+
 
   async function addPost() {
-    await addDoc(collection(db, "posts"), { caption, image });
+    const imageReference = ref(storage, `images/${image.name}`);
+    const response = await uploadBytes(imageReference, image);
+    const imageUrl = await getDownloadURL(response.ref);
+    await addDoc(collection(db, "posts"), { caption, image: imageUrl });
     navigate("/");
   }
 
-  useEffect(() => { 
+
+  useEffect(() => {
     if (loading) return;
     if (!user) return navigate("/login");
   }, [navigate, user, loading]);
@@ -47,18 +55,26 @@ export default function PostPageAdd() {
               onChange={(text) => setCaption(text.target.value)}
             />
           </Form.Group>
+          <Image
+            src={previewImage}
+            style={{
+              objectFit: "cover",
+              width: "10rem",
+              height: "10rem",
+            }}
+          />
 
           <Form.Group className="mb-3" controlId="image">
-            <Form.Label>Image URL</Form.Label>
+            <Form.Label>Image</Form.Label>
             <Form.Control
-              type="text"
-              placeholder="https://zca.sg/img/1"
-              value={image}
-              onChange={(text) => setImage(text.target.value)}
+              type="file"
+              onChange={(e) => {
+                const imageFile = e.target.files[0]
+                setImage(imageFile)
+                const selectedImage = URL.createObjectURL(e.target.files[0]);
+                setPreviewImage(selectedImage);
+              }}
             />
-            <Form.Text className="text-muted">
-              Make sure the url has a image type at the end: jpg, jpeg, png.
-            </Form.Text>
           </Form.Group>
           <Button variant="primary" onClick={async (e) => addPost()}>
             Submit
